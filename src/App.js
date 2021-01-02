673f08f2acab02cb41045f1250bda33d2e5e6f6c
Кайрат Давранбеков <kdavranbekov@gmail.com>
2021-01-02 17:12:56 +0600

    header component built

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Title from './Title'
+import './css/App.css'
 
 function App() {
   return (
       <div className='App'>
+        <Title />
       </div>
   );
 }
