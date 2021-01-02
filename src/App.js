0235d740857b1b33fcf42240349420f282c5913c
Кайрат Давранбеков <kdavranbekov@gmail.com>
2021-01-02 19:09:09 +0600

    content component built

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
+
 import Title from './Title'
+import POPOList from './POPOSList'
+
 import './css/App.css'
 
 function App() {
   return (
       <div className='App'>
         <Title />
+        <POPOList />
       </div>
   );
 }
